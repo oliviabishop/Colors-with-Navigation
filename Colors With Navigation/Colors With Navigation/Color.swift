707068923088ab6580246098058659f26f724218
Olivia Bishop <oliviabishop@Olivias-MacBook-Pro-3.local>
2018-03-19 15:29:01 -0500

    first commit

---
+++ Colors With Navigation/Colors With Navigation/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  Colors With Navigation
+//
+//  Created by Olivia Bishop on 3/19/18.
+//  Copyright © 2018 Olivia Bishop. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    
+    let name: String
+    let uiColor : UIColor
+    
+    
+}
---
+++ Colors With Navigation/Colors With Navigation/ColorDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ColorDetailViewController.swift
+//  Colors With Navigation
+//
+//  Created by Olivia Bishop on 3/19/18.
+//  Copyright © 2018 Olivia Bishop. All rights reserved.
+//
+
+import UIKit
+
+class ColorDetailViewController: UIViewController {
+    
+    var color: Color?
+    @IBOutlet weak var colorNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorNameLabel.text = color?.name
+        self.view.backgroundColor = color?.uiColor
+        self.title = color?.name
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
